b3177d129a32f3ff1df6a0d737c768d515b41889
c4lyp5o <calypso@calypsocloud.one>
2023-11-09 13:55:58 +0800

    fix: buttons
    tailwind ui

--- components/buttons.js
+++ components/buttons.js
@@ -1,34 +1,32 @@
 export function BusyButton() {
   return (
-    <>
-      <button
-        type='button'
-        className='inline-flex items-center text-center justify-center px-4 py-2 bg-green-400 rounded-md shadow-xl p-2 hover:bg-green-600 transition-all ease-in-out duration-150 cursor-not-allowed'
-        disabled
+    <button
+      type='button'
+      className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2 cursor-not-allowed'
+      disabled
+    >
+      <svg
+        className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+        xmlns='http://www.w3.org/2000/svg'
+        fill='none'
+        viewBox='0 0 24 24'
       >
-        <svg
-          className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-        >
-          <circle
-            className='opacity-25'
-            cx='12'
-            cy='12'
-            r='10'
-            stroke='currentColor'
-            strokeWidth='4'
-          ></circle>
-          <path
-            className='opacity-75'
-            fill='currentColor'
-            d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-          ></path>
-        </svg>
-        Menambah Data...
-      </button>
-    </>
+        <circle
+          className='opacity-25'
+          cx='12'
+          cy='12'
+          r='10'
+          stroke='currentColor'
+          strokeWidth='4'
+        ></circle>
+        <path
+          className='opacity-75'
+          fill='currentColor'
+          d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+        ></path>
+      </svg>
+      Menambah Data...
+    </button>
   );
 }
 
@@ -36,7 +34,7 @@ export function SubmitButton({ func }) {
   return (
     <button
       type='submit'
-      className='capitalize bg-green-400 rounded-md shadow-xl p-2 hover:bg-green-600 transition-all'
+      className='inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:col-start-2 capitalize'
     >
       {func === 'add' && 'Tambah Data'}
       {func === 'edit' && 'Kemaskini Data'}
