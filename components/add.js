432e048be2d2a3ad6a7e407051067ae6b49bf7eb
c4lyp5o <calypso@calypsocloud.one>
2023-11-09 13:54:40 +0800

    fix: add modal
    tailwind ui

--- components/add.js
+++ components/add.js
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import { RiCloseLine } from 'react-icons/ri';
+import { Fragment, useRef, useState } from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+
 import axios from 'axios';
 
 import { toast } from 'react-toastify';
 import { Human, NonHuman } from './inputs';
 import { BusyButton, SubmitButton } from './buttons';
-import styles from '../styles/Modal.module.css';
 
-const Modal = ({ toggle, setOpenAddModal, mutate }) => {
+const Modal = ({ toggle, openAddModal, setOpenAddModal, mutate }) => {
   const [slate, setSlate] = useState({});
   const [addingData, setAddingData] = useState(false);
+
+  const cancelButtonRef = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -19,60 +21,62 @@ const Modal = ({ toggle, setOpenAddModal, mutate }) => {
       type: toggle,
       nama: slate.nama,
     };
-    if (toggle === 'pegawai') {
-      if (slate.mdcNumber.match(/[^0-9]/)) {
-        toast.error('MDC Number hanya boleh mengandungi nombor');
-        setAddingData(false);
-        return;
-      }
-      Data = {
-        ...Data,
-        mdcNumber: slate.mdcNumber,
-        statusPegawai: 'pp',
-      };
+    switch (toggle) {
+      case 'pegawai':
+        if (slate.mdcNumber.toString().match(/[^0-9]/)) {
+          toast.error('Nombor MDC hanya boleh mengandungi nombor');
+          setAddingData(false);
+          return;
+        }
+        Data = {
+          ...Data,
+          mdcNumber: slate.mdcNumber,
+          statusPegawai: 'pp',
+        };
+        break;
+      case 'juruterapi':
+        if (!slate.mdtbNumber.match(/^(MDTB|mdtb)/)) {
+          toast.error('Nombor MDTB mesti diawali dengan MDTB');
+          setAddingData(false);
+          return;
+        }
+        if (slate.mdtbNumber.match(/^(mdtb)/)) {
+          slate.mdtbNumber = slate.mdtbNumber.toUpperCase();
+        }
+        Data = {
+          ...Data,
+          mdtbNumber: slate.mdtbNumber,
+          statusPegawai: 'jp',
+        };
+        break;
+      case 'fasiliti':
+        Data = {
+          ...Data,
+          daerah: slate.daerah,
+          negeri: slate.negeri,
+          kodFasiliti: slate.kodFasiliti,
+          kodFasilitiGiret: slate.kodFasilitiGiret,
+        };
+        break;
+      case 'kkiakd':
+        Data = {
+          ...Data,
+          namaHospital: slate.namaHospital,
+          daerah: slate.daerah,
+          negeri: slate.negeri,
+          kodFasiliti: slate.kodFasiliti,
+          jenisFasiliti: slate.jenisFasiliti,
+        };
+        break;
+      default:
+        break;
     }
-    if (toggle === 'juruterapi') {
-      if (!slate.mdtbNumber.match(/^(MDTB|mdtb)/)) {
-        toast.error('MDTB Number mesti diawali dengan MDTB');
-        setAddingData(false);
-        return;
-      }
-      if (slate.mdtbNumber.match(/^(mdtb)/)) {
-        slate.mdtbNumber = slate.mdtbNumber.toUpperCase();
-      }
-      Data = {
-        ...Data,
-        mdtbNumber: slate.mdtbNumber,
-        statusPegawai: 'jp',
-      };
-    }
-    if (toggle === 'fasiliti') {
-      Data = {
-        ...Data,
-        daerah: slate.daerah,
-        negeri: slate.negeri,
-        kodFasiliti: slate.kodFasiliti,
-        kodFasilitiGiret: slate.kodFasilitiGiret,
-      };
-    }
-    if (toggle === 'kkiakd') {
-      Data = {
-        ...Data,
-        namaHospital: slate.namaHospital,
-        daerah: slate.daerah,
-        negeri: slate.negeri,
-        kodFasiliti: slate.kodFasiliti,
-        jenisFasiliti: slate.jenisFasiliti,
-      };
-    }
-    console.log(Data);
     try {
-      const res = await axios.post('/api/gpass', {
+      await axios.post('/api/gpass', {
         query: 'create',
         payload: Data,
       });
       toast.success('Data berjaya ditambah');
-      console.log(res);
     } catch (err) {
       toast.error('Data gagal ditambah');
       console.log(err);
@@ -89,42 +93,63 @@ const Modal = ({ toggle, setOpenAddModal, mutate }) => {
   };
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div className={styles.darkBG} onClick={() => setOpenAddModal(false)} />
-        <div className={styles.centered}>
-          <div className={styles.modalAdd}>
-            <div className={styles.modalHeader}>
-              <h5 className={styles.heading}>Tambah {toggle}</h5>
-            </div>
-            <span
-              className={styles.closeBtn}
-              onClick={() => setOpenAddModal(false)}
+    <Transition.Root show={openAddModal} as={Fragment}>
+      <Dialog
+        as='div'
+        className='relative z-10'
+        initialFocus={cancelButtonRef}
+        onClose={setOpenAddModal}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <RiCloseLine style={{ marginBottom: '-3px' }} />
-            </span>
-            <div className={styles.modalContent}>
-              {toggle !== 'fasiliti' && toggle !== 'kkiakd' ? (
-                <Human {...InputProps} />
-              ) : (
-                <NonHuman {...InputProps} />
-              )}
-            </div>
-            <div className={styles.modalActions}>
-              <div className={styles.actionsContainer}>
-                {addingData ? <BusyButton /> : <SubmitButton func='add' />}
-                <span
-                  className={styles.cancelBtn}
-                  onClick={() => setOpenAddModal(false)}
-                >
-                  Cancel
-                </span>
-              </div>
-            </div>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    {toggle !== 'fasiliti' && toggle !== 'kkiakd' ? (
+                      <Human {...InputProps} />
+                    ) : (
+                      <NonHuman {...InputProps} />
+                    )}
+                  </div>
+                  <div className='mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3'>
+                    {addingData ? <BusyButton /> : <SubmitButton func='add' />}
+                    <button
+                      type='button'
+                      className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0'
+                      onClick={() => setOpenAddModal(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Batal
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
-      </form>
-    </>
+      </Dialog>
+    </Transition.Root>
   );
 };
 
