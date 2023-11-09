71aa31e0a2be868ed29330f600896dac6194cab1
c4lyp5o <calypso@calypsocloud.one>
2023-11-09 13:54:56 +0800

    fix: edit modal
    tailwind ui

--- components/edit.js
+++ components/edit.js
@@ -1,21 +1,22 @@
-import { useState, useEffect } from 'react';
+import { Fragment, useRef, useState, useEffect } from 'react';
 import useSWR from 'swr';
+import { Dialog, Transition } from '@headlessui/react';
+
 import axios from 'axios';
-import { RiCloseLine } from 'react-icons/ri';
 
 import Loading from './loading';
+import Toast from './toast';
 
 import { toast } from 'react-toastify';
 import { Human, NonHuman } from './inputs';
 import { BusyButton, SubmitButton } from './buttons';
-import styles from '../styles/Modal.module.css';
 
 async function fetcher(url) {
   const res = await fetch(url);
   return res.json();
 }
 
-const Modal = ({ toggle, setOpenEditModal, entity, mutate }) => {
+const Modal = ({ toggle, openEditModal, setOpenEditModal, entity, mutate }) => {
   const { data, error } = useSWR(
     `/api/gpass?type=individu&from=${toggle}&id=${entity.bil}`,
     fetcher
@@ -23,6 +24,8 @@ const Modal = ({ toggle, setOpenEditModal, entity, mutate }) => {
 
   const [slate, setSlate] = useState({});
   const [editingData, setEditingData] = useState('');
+
+  const cancelButtonRef = useRef(null);
 
   useEffect(() => {
     if (data) {
@@ -39,56 +42,59 @@ const Modal = ({ toggle, setOpenEditModal, entity, mutate }) => {
       type: toggle,
       bil: entity.bil,
     };
-    if (toggle === 'pegawai') {
-      if (slate.mdcNumber.match(/[^0-9]/)) {
-        toast.error('MDC Number hanya boleh mengandungi nombor');
-        setEditingData(false);
-        return;
-      }
-      Data = {
-        ...Data,
-        updateMdcNumber: slate.mdcNumber,
-      };
+    switch (toggle) {
+      case 'pegawai':
+        if (slate.mdcNumber.toString().match(/[^0-9]/)) {
+          toast.error('Nombor MDC hanya boleh mengandungi nombor');
+          setEditingData(false);
+          return;
+        } else {
+          Data = {
+            ...Data,
+            updateMdcNumber: slate.mdcNumber,
+          };
+        }
+        break;
+      case 'juruterapi':
+        if (!slate.mdtbNumber.match(/^(MDTB|mdtb)/)) {
+          toast.error('Nombor MDTB mesti diawali dengan MDTB');
+          setEditingData(false);
+          return;
+        }
+        if (slate.mdtbNumber.match(/^(mdtb)/)) {
+          slate.mdtbNumber = slate.mdtbNumber.toUpperCase();
+        }
+        Data = {
+          ...Data,
+          updateMdtbNumber: slate.mdtbNumber,
+        };
+        break;
+      case 'fasiliti':
+        Data = {
+          ...Data,
+          updateDaerah: slate.daerah,
+          updateNegeri: slate.negeri,
+          updateKodFasiliti: slate.kodFasiliti,
+          updateKodFasilitiGiret: slate.kodFasilitiGiret,
+        };
+        break;
+      case 'kkiakd':
+        Data = {
+          ...Data,
+          updateNamaHospital: slate.namaHospital,
+          updateKodFasiliti: slate.kodFasiliti,
+          updateJenisFasiliti: slate.jenisFasiliti,
+        };
+        break;
+      default:
+        break;
     }
-    if (toggle === 'juruterapi') {
-      if (!slate.mdtbNumber.match(/^(MDTB|mdtb)/)) {
-        toast.error('MDTB Number mesti diawali dengan MDTB');
-        setAddingData(false);
-        return;
-      }
-      if (slate.mdtbNumber.match(/^(mdtb)/)) {
-        slate.mdtbNumber = slate.mdtbNumber.toUpperCase();
-      }
-      Data = {
-        ...Data,
-        updateMdtbNumber: slate.mdtbNumber,
-      };
-    }
-    if (toggle === 'fasiliti') {
-      Data = {
-        ...Data,
-        updateDaerah: slate.daerah,
-        updateNegeri: slate.negeri,
-        updateKodFasiliti: slate.kodFasiliti,
-        updateKodFasilitiGiret: slate.kodFasilitiGiret,
-      };
-    }
-    if (toggle === 'kkiakd') {
-      Data = {
-        ...Data,
-        updateNamaHospital: slate.namaHospital,
-        updateKodFasiliti: slate.kodFasiliti,
-        updateJenisFasiliti: slate.jenisFasiliti,
-      };
-    }
-    console.log(Data);
     try {
-      const res = await axios.post('/api/gpass', {
+      await axios.post('/api/gpass', {
         query: 'update',
         payload: Data,
       });
       toast.success('Data berjaya dikemaskini');
-      console.log(res);
     } catch (err) {
       toast.error('Data gagal dikemaskini');
       console.log(err);
@@ -108,45 +114,67 @@ const Modal = ({ toggle, setOpenEditModal, entity, mutate }) => {
   if (error) return <div>failed to load</div>;
 
   return (
-    <>
-      <form onSubmit={handleSubmit}>
-        <div
-          className={styles.darkBG}
-          onClick={() => setOpenEditModal(false)}
-        />
-        <div className={styles.centered}>
-          <div className={styles.modalAdd}>
-            <div className={styles.modalHeader}>
-              <h5 className={styles.heading}>Ubah {toggle}</h5>
-            </div>
-            <span
-              className={styles.closeBtn}
-              onClick={() => setOpenEditModal(false)}
+    <Transition.Root show={openEditModal} as={Fragment}>
+      <Dialog
+        as='div'
+        className='relative z-10'
+        initialFocus={cancelButtonRef}
+        onClose={setOpenEditModal}
+      >
+        <Transition.Child
+          as={Fragment}
+          enter='ease-out duration-300'
+          enterFrom='opacity-0'
+          enterTo='opacity-100'
+          leave='ease-in duration-200'
+          leaveFrom='opacity-100'
+          leaveTo='opacity-0'
+        >
+          <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity' />
+        </Transition.Child>
+
+        <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+          <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+            <Transition.Child
+              as={Fragment}
+              enter='ease-out duration-300'
+              enterFrom='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
+              enterTo='opacity-100 translate-y-0 sm:scale-100'
+              leave='ease-in duration-200'
+              leaveFrom='opacity-100 translate-y-0 sm:scale-100'
+              leaveTo='opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95'
             >
-              <RiCloseLine style={{ marginBottom: '-3px' }} />
-            </span>
-            <div className={styles.modalContent}>
-              {toggle !== 'fasiliti' && toggle !== 'kkiakd' ? (
-                <Human {...InputProps} />
-              ) : (
-                <NonHuman {...InputProps} />
-              )}
-            </div>
-            <div className={styles.modalActions}>
-              <div className={styles.actionsContainer}>
-                {editingData ? <BusyButton /> : <SubmitButton func='edit' />}
-                <span
-                  className={styles.cancelBtn}
-                  onClick={() => setOpenEditModal(false)}
-                >
-                  Cancel
-                </span>
-              </div>
-            </div>
+              <Dialog.Panel className='relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6'>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    {toggle !== 'fasiliti' && toggle !== 'kkiakd' ? (
+                      <Human {...InputProps} />
+                    ) : (
+                      <NonHuman {...InputProps} />
+                    )}
+                  </div>
+                  <div className='mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3'>
+                    {editingData ? (
+                      <BusyButton />
+                    ) : (
+                      <SubmitButton func='edit' />
+                    )}
+                    <button
+                      type='button'
+                      className='mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:col-start-1 sm:mt-0'
+                      onClick={() => setOpenEditModal(false)}
+                      ref={cancelButtonRef}
+                    >
+                      Batal
+                    </button>
+                  </div>
+                </form>
+              </Dialog.Panel>
+            </Transition.Child>
           </div>
         </div>
-      </form>
-    </>
+      </Dialog>
+    </Transition.Root>
   );
 };
 
