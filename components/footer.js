d121480a7aaa6519b8ca864765bf0f38812fe7a9
c4lyp5o <calypso@calypsocloud.one>
2024-02-09 16:29:34 +0800

    chore: delete footer

--- components/footer.js
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <div className='absolute bg-green-400 bottom-0 left-0 right-0 grid grid-cols-2 uppercase'>
-      <span className='text-left ml-1 my-1 text-xs'>
-        hak cipta kementerian kesihatan malaysia
-      </span>
-    </div>
-  );
-}
