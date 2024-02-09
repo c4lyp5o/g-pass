9faf93c27bd87487300b42de7f86c52e906651f6
c4lyp5o <calypso@calypsocloud.one>
2024-02-09 16:30:36 +0800

    feat: prisma client for global

---
+++ database/prismaClient.js
@@ -0,0 +1,16 @@
+import { PrismaClient } from '@prisma/client';
+
+let prisma;
+
+//check if we are running in production mode
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  //check if there is already a connection to the database
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
