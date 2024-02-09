fea39ccf6d90dd6f3dc816fd26835393e216e0e2
c4lyp5o <calypso@calypsocloud.one>
2024-02-09 16:35:17 +0800

    fix: nextauth route using global prisma

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,29 +2,27 @@ import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
+import { prisma } from '../../../database/prismaClient';
 
 export const authOptions = {
   providers: [
     // REAL mailer
-    EmailProvider({
-      server: process.env.SMTP_HOST,
-      from: process.env.SMTP_FROM,
-    }),
-    // OG mailer
     // EmailProvider({
-    //   server: {
-    //     host: process.env.SMTP_HOST,
-    //     port: Number(process.env.SMTP_PORT),
-    //     auth: {
-    //       user: process.env.SMTP_USER,
-    //       pass: process.env.SMTP_PASSWORD,
-    //     },
-    //   },
+    //   server: process.env.SMTP_HOST,
     //   from: process.env.SMTP_FROM,
     // }),
+    // OG mailer
+    EmailProvider({
+      server: {
+        host: process.env.SMTP_HOST,
+        port: Number(process.env.SMTP_PORT),
+        auth: {
+          user: process.env.SMTP_USER,
+          pass: process.env.SMTP_PASSWORD,
+        },
+      },
+      from: process.env.SMTP_FROM,
+    }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
   adapter: PrismaAdapter(prisma),
